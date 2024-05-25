0bbcc0a7e77ca9a40fb4f1697a08209c4d2e8dfa
Pmorilla <pmorillaf@gmail.com>
2024-05-25 22:44:49 +0200

    Versión refactorizada: separación de la lógica de suma en un método

--- sumaNum/src/sumaNum/SumaNumeros.java
+++ sumaNum/src/sumaNum/SumaNumeros.java
@@ -1,14 +1,19 @@
 package sumaNum;
 
-public class SumaNumeros 
-{
-	public static void main(String[] args) {
-        int n = 10; // Número de elementos a sumar
+public class SumaNumeros {
+    public static void main(String[] args) {
+        int cantidadNumeros = 10; // Número de elementos a sumar
+        int resultadoSuma = calcularSuma(cantidadNumeros);
+        System.out.println("La suma de los primeros " + cantidadNumeros + 
+        		" números naturales es: " + resultadoSuma);
+    }
+
+    public static int calcularSuma(int n) {
         int suma = 0;
         for (int i = 1; i <= n; i++) {
             suma += i;
         }
-        System.out.println("La suma de los primeros " + n + 
-        		" números naturales es: " + suma);
+        return suma;
     }
 }
+
